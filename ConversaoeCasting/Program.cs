fed7345cc67e20bc80c2e5a20eefa1fec167d10a
ArthurFoltz <arthurmarianoff@hotmail.com>
2022-08-24 20:45:05 -0300

    Add 4 classes
    Conversao dados, casting, atribuiçao e operadores aritmeticos

---
+++ ConversaoeCasting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConversaoeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+
+            double a;
+            int b;
+            a = 5.1;
+            b = (int)a;
+            Console.WriteLine(b);
+
+            int a = 5;
+            int b = 2;
+            double resultado = (double)a / b;
+            Console.WriteLine(resultado);
+
+
+
+        }
+    }
+}
---
+++ EntradaDados/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
